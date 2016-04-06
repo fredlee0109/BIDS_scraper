32cdd4e361b73202232abb7714e88f7f72d2edac
Fred Lee <lfredl109@gmail.com>
2016-04-05 23:48:19 -0700

    some changes

--- api.py
+++ api.py
@@ -12,13 +12,33 @@ import API_KEY as API_KEY
 class fbAPI(object):
 
 	def __init__(self, link):
-		start = link.index('facebook.com/') + 13
-		end = link[start:].find('/') + start
+		start = link.find('facebook.com/') + 13
+		if start == -1:
+			self.page = None
+			return
+		if link[start:].find('/') == -1:
+			end = 5
+		else:
+			end = link[start:].find('/') + start
 		self.page = link[start:end]
 
 
 	def post_date_diff(self):
-		graph = facebook.GraphAPI(API_KEY.key)
+		if self.page == None:
+			return "No facebook.com detected"
+		graph = facebook.GraphAPI(API_KEY.facebook)
+		# print graph.get_connections(graph.get_object(self.page)['id'], 'bio')
+		# print graph.get_connections(graph.get_object(self.page)['id'], 'posts')
+		print graph.get_object(self.page)
 		posts = graph.get_connections(self.page, 'posts')
+		print '========='
+		# print posts
 		date = datetime.strptime(posts['data'][0]['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
-		print (datetime.today() - date).total_seconds()
+		return (datetime.today() - date).total_seconds()
+
+class twitterAPI(object):
+
+	def __init__(self, link):
+		start = link.find('twitter.com/') + 12
+		end = link[start:].find('/') + start
+		self.page = link[start:end]
--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 def main():
 	origin = 'https://www.facebook.com/advanotech/timeline?ref=page_internal'
-	api.fbAPI(origin).post_date_diff()
+	print api.fbAPI(origin).post_date_diff()
 
 	# time.sleep(60)
 
