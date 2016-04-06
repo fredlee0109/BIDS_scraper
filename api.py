9aa63d628c4ccba23dbc1327d3588f5fa5e8b781
Fred Lee <lfredl109@gmail.com>
2016-04-05 23:51:11 -0700

    some changes

--- api.py
+++ api.py
@@ -27,9 +27,8 @@ class fbAPI(object):
 		if self.page == None:
 			return "No facebook.com detected"
 		graph = facebook.GraphAPI(API_KEY.facebook)
-		# print graph.get_connections(graph.get_object(self.page)['id'], 'bio')
-		# print graph.get_connections(graph.get_object(self.page)['id'], 'posts')
-		print graph.get_object(self.page)
+		
+		print graph.get_object(self.page)['id']
 		posts = graph.get_connections(self.page, 'posts')
 		print '========='
 		# print posts
